90eb9d88474e454e7873ac361bfa91dc2b0c6143
take-cantik <take.cantik17@gmail.com>
2022-06-04 15:53:49 +0900

    Delete: follow and other event

--- functions/src/triggers/https/line-bot/handlers/index.ts
+++ functions/src/triggers/https/line-bot/handlers/index.ts
@@ -2,15 +2,12 @@ import { WebhookEvent } from '@line/bot-sdk'
 import { lineClient } from '~/utils/line'
 import { msgError } from '~line/notice-messages/other'
 
-import { followHandler } from './follow'
 import { messagesHandler } from './messages'
 import { errorLogger } from '~/utils/util'
 
 export const handlers = async (event: WebhookEvent): Promise<void> => {
   try {
     switch (event.type) {
-      case 'follow':
-        return await followHandler(event)
       case 'message':
         return await messagesHandler(event)
       default:
