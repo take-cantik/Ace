a3ce25128f11ff4c7d79a42ca5f28ba5fb96ea34
take-cantik <take.cantik17@gmail.com>
2022-06-07 17:06:31 +0900

    Update: change error action and delete follow message

--- functions/src/triggers/https/line-bot/notice-messages/follow.ts
+++
@@ -1,6 +0,0 @@
-import { TextMessage } from '@line/bot-sdk'
-
-export const msgFollow: TextMessage = {
-  type: 'text',
-  text: 'フォローありがとう！'
-}
--- functions/src/utils/secrets.ts
+++ functions/src/utils/secrets.ts
@@ -9,3 +9,7 @@ export const LINE_MESSAGING_CHANNEL_SECRET = process.env.LINE_MESSAGING_CHANNEL_
 
 export const DATABASE_URL = process.env.DATABASE_URL as string
 export const STORAGE_BUCKET = process.env.STORAGE_BUCKET as string
+
+// other
+
+export const MY_LINE_LINK = process.env.MY_LINE_LINK as string
